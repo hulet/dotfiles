dad042f69ea7216276a31a936b4ad58968961c25
Steve Hulet <steve@freshconsulting.com>
2026-01-13 13:27:57 -0800

    switch from vim to neovim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,4 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- no autoformatting, please
 vim.g.autoformat = false
+
+-- use absolute line numbers
+vim.opt.relativenumber = false
+
+-- no filler character for deleted lines in diff mode
+vim.opt.fillchars:append({ diff = " " })
---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      on_highlights = function(hl, c)
+        -- DiffChange: The background for the whole modified line (Subtle)
+        hl.DiffChange = { bg = "#2e334d" }
+
+        -- DiffText: The specific changed characters (High Contrast)
+        -- We'll give it a brighter background and a bold font
+        hl.DiffText = { bg = "#44508a", fg = "#ff9e64", bold = true }
+      end,
+    },
+  },
+}
